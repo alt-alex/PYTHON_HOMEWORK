120d7e038a5280d45d6e829a5f44a4ca132a267e
Alexey <pstbx9.2@gmail.com>
2019-11-20 00:30:45 +0300

    answ for 5, 5-3

--- 05_basic_scripts/access_trunk.py
+++ 05_basic_scripts/access_trunk.py
@@ -1,27 +1,29 @@
+#!/usr/bin/env python
+
 a = input('Введите режим работы интерфейса (access/trunk): ')
 
 b = input('Введите тип и номер интерфейса: ')
 
 c = input('Введите номер влан(ов): ')
 
-
-
-
 access_template = '''
-    'interface {}',
-     'switchport mode access', 'switchport access vlan {}',
-     'switchport nonegotiate', 'spanning-tree portfast',
-     'spanning-tree bpduguard enable'
+     interface {}
+     switchport mode access
+     switchport access vlan {}
+     switchport nonegotiate
+     spanning-tree portfast
+     spanning-tree bpduguard enable
      '''
 
-print(access_template.format(b, c))
-
-
-
- trunk_template = '''
-    'interface{}',
-    'switchport trunk encapsulation dot1q', 'switchport mode trunk',
-    'switchport trunk allowed vlan {}'
+trunk_template = '''
+    interface {}
+    switchport trunk encapsulation dot1q
+    switchport mode trunk
+    switchport trunk allowed vlan {}
     '''
 
-print(trunk_template(b,c))
+
+access_trunk = {'access': access_template , 'trunk': trunk_template}
+
+
+print(access_trunk[a].format(b, c))
