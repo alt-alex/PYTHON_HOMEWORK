2596b12a15563f1af3cac043a9a5c095c3ad5e8c
Alexey <pstbx9.2@gmail.com>
2020-01-12 22:55:13 +0300

    20_2 and 20_3

--- 20_concurrent_connections/answ_for_task_20_2.py
+++ 20_concurrent_connections/answ_for_task_20_2.py
@@ -1,32 +1,37 @@
-#!/usr/bin env python
+#!/usr/bin/env python
 
-import yaml
-
-from itertools import repeat
-from concurrent.futures import ThreadPoolExecutor
-
-
-with open('devices.yaml') as f:
-    dev = yaml.safe_load(f)
-
-def con(d_p, CMD):
+def con(dev_prmt, CMD):
     import netmiko
-    with netmiko.ConnectHandler(**dev_prmts) as tn:
+    with netmiko.ConnectHandler(**dev_prmt) as tn:
         tn.enable()
         result = tn.send_command(CMD)
-        print("we get" + result)
+        print("we get" + result  + ' from ' + dev_prmt['ip'])
         return result
 
 
-for dev_prmts in dev:
-    con(dev_prmts, 'sh clock')
+def send_show_command_to_devices(DEV='devices.yaml',
+                                 CMD='sh clock',
+                                 FILENAME='new_file.yaml',
+                                 LMT=3):
+    from itertools import repeat
+    import yaml, datetime
+    from concurrent.futures import ThreadPoolExecutor
 
+    with open(DEV) as f:
+        dvs = yaml.safe_load(f)
 
+    with ThreadPoolExecutor(max_workers=int(LMT)) as executor:
+        hello_line = 'Create new file'
+        with open(FILENAME, 'w') as f:
+            yaml.dump(hello_line, f)
+        start_time = datetime.datetime.now()
+        result = executor.map(con, dvs, repeat(CMD))
+        for d, outp in zip(dvs, result):
+            line = (d['ip'], outp)
+            with open(FILENAME, 'a') as f:
+                yaml.dump(line, f)
+        t = datetime.datetime.now() - start_time
+        print('program execution time ' + str(t))
 
-with ThreadPoolExecutor(max_workers=3) as executor:
-    result = executor.map(con, dev, repeat('sh clock'))
-    for d, outp in zip(dev, result):
-        line = (d['ip'], outp)
-        with open('new_file.yaml', 'a') as f:
-            yaml.dump(line, f)
-
+if __name__ == "__main__":
+    send_show_command_to_devices()
